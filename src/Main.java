702045b44e6b7f0caa2cb864d75a00d6f5938eab
bdyeazel <bdyeazel@gmail.com>
2026-06-08 19:52:16 -0400

    Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,61 @@
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) // how to start java coding for class
     {
+    int intA = 8; // int are whole numbers
+    int intB = 3;
+    int intSum = 0;
+    double doubleD = 7.0; //double are numbers with a decimal in them
+    double doubleE = 9.5;
+    double doubleSum = 0.0;
 
+    intSum = intA + intB;
+    System.out.print("The sum of " + intA + " and " + intB + " is " + intSum); //output statement
+
+    intSum = intA - intB;
+    System.out.print(" The sum of " + intA + " and " + intB + " is " + intSum);
+
+    intSum = intA * intB;
+    System.out.print(" The sum of " + intA + " and " + intB + " is " + intSum);
+
+    intSum = intA / intB;
+    System.out.print(" The sum of " + intA + " and " + intB + " is " + intSum);
+
+    intSum = intA % intB;
+    System.out.print(" The sum of " + intA + " and " + intB + " is " + intSum);
+
+    doubleSum = doubleD + doubleE;
+    System.out.print("The sum of " + doubleD + " and " + doubleE + " is " + doubleSum); //output statement
+
+    doubleSum = doubleD - doubleE;
+    System.out.print("The sum of " + doubleD + " and " + doubleE + " is " + doubleSum);
+
+    doubleSum = doubleD * doubleE;
+    System.out.print("The sum of " + doubleD + " and " + doubleE + " is " + doubleSum);
+
+    doubleSum = doubleD / doubleE;
+    System.out.print("The sum of " + doubleD + " and " + doubleE + " is " + doubleSum);
+
+    doubleSum = doubleD % doubleE;
+    System.out.print("The sum of " + doubleD + " and " + doubleE + " is " + doubleSum);
+
+    double restaurantBill = 18.75;
+    int siblingsCount = 1;
+    boolean rainingOutside = true;
+    double fuelPrice = 3.45;
+    int luckyNumber = 13;
+    double sneakerSize = 11.0;
+    int birthdayMonth = 5;
+    String studentName = "Brad Yeazel";
+
+    System.out.println("Restaurant bill: $" + restaurantBill);
+    System.out.println("Number of siblings: " + siblingsCount);
+    System.out.println("Is it raining outside? " + rainingOutside);
+    System.out.println("Fuel price: $" + fuelPrice);
+    System.out.println("Lucky number: " + luckyNumber);
+    System.out.println("Sneaker size: " + sneakerSize);
+    System.out.println("Birthday month: " + birthdayMonth);
+    System.out.println("Student name: " + studentName);
 
 
     }
